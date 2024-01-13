5f868f2ee8a4adaab958f543415da155d80ecb63
PawelKulfan <qlartstudio@gmail.com>
2024-01-13 09:54:36 +0100

    First test and configuration

---
+++ src/test/java/pl/coderslab/FirstTest.java
@@ -0,0 +1,14 @@
+package pl.coderslab;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FirstTest {
+
+    @Test
+    public void firstTest(){
+        assertEquals(1, 1);
+    }
+
+}
