fa89f6f90b53110b73fb81d872fc026ee059b406
PawelKulfan <qlartstudio@gmail.com>
2024-01-13 13:01:21 +0100

    Second part of excercise

--- src/test/java/pl/coderslab/FirstTest.java
+++ src/test/java/pl/coderslab/FirstTest.java
@@ -1,14 +1,49 @@
 package pl.coderslab;
 
-import org.junit.Test;
+import org.junit.*;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class FirstTest {
 
+    @BeforeClass
+    public static void setUpClass(){
+        System.out.println("setUpClass");
+    }
+
+    @AfterClass
+    public static void tearDownClass(){
+        System.out.println("tearDownClass");
+    }
+
+    @Before
+    public void setUp(){
+        System.out.println("setUp");
+    }
+
+    @After
+    public void tearDown(){
+        System.out.println("tearDown");
+    }
+
     @Test
-    public void firstTest(){
-        assertEquals(1, 1);
+    public void failedTest(){
+//        assert 1 == 2;
+        System.out.println("failedTest");
+        assertEquals("1 ma sie rownac jeden ale: ",1, 2);
+    }
+
+    @Test
+    public void passTest(){
+        System.out.println("passTest");
+        assertTrue(true);
+    }
+
+    @Test
+    public void brokenTest(){
+        System.out.println("brokenTest");
+        throw new RuntimeException();
     }
 
 }
